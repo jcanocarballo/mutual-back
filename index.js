03cc998082f5ee71d34d467e303ef2424dfb3585
Julian Cano <jcanocarballo@gmail.com>
2021-05-11 16:29:39 -0500

    Se agrega ruta statica para login

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.use(express.json());
 
 //Rutas
 app.use('/login', express.static('public'));
+app.use('/auth/login', express.static('public'));
 //TODO: auth -> crear, login, renew
 app.use('/api/auth', require('./routes/auth'));
 //TODO: CRUD: -> Eventos
